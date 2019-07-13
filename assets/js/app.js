52b0bcaa78ac29969a394bfa07c217ef3180e61f
lufficc <luffy.lcc@gmail.com>
2019-07-14 03:46:02 +0800

    add bibtex

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+function selectElementContents(el) {
+    if (window.getSelection && document.createRange) {
+        var sel = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (document.selection && document.body.createTextRange) {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.select();
+    }
+}
+
+var dialog = document.querySelector('dialog');
+if (!dialog.showModal) {
+  dialogPolyfill.registerDialog(dialog);
+}
+var bibtexs = document.querySelectorAll('.bibtex');
+for (var i = 0; i < bibtexs.length; ++i) {
+    bibtex = bibtexs[i];
+    bibtex.addEventListener('click', function() {
+        var html = document.querySelector('#' + this.getAttribute('bibtex-id')).innerHTML
+        var dialog_content = document.querySelector('#dialog-content');
+        dialog_content.innerHTML = html;
+        selectElementContents(dialog_content);
+        dialog.showModal();
+    });
+
+}
+
+dialog.querySelector('.close').addEventListener('click', function() {
+  dialog.close();
+});
